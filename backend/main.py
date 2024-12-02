03460dd32c3caac5af913a13aa7b173b273952b2
Justin Lee <justinkleecs@gmail.com>
2024-12-02 16:26:22 -0500

    trailing slash bug

--- backend/main.py
+++ backend/main.py
@@ -43,7 +43,7 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://truthrisenews.netlify.app/"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "https://truthrisenews.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
